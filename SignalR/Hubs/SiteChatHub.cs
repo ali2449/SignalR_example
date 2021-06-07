73a9f7f2b952058c7af9564a6af0eabda4343a54
Ali Noori <alinoori@outlook.com>
2021-06-07 21:15:59 +0430

    Add project files.

---
+++ SignalR/Hubs/SiteChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class SiteChatHub:Hub
+    {
+        public async Task SendNewMessage(string sender,string Message)
+        {
+          await  Clients.All.SendAsync("getNewMessage", sender, Message, DateTime.Now.ToShortDateString());
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
